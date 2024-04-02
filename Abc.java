b5f62dbcf794bc86f00156c2370b6a8b4830b1ba
amitnaik9922 <amitnaikwade9922@gmail.com>
2024-04-02 15:37:19 +0530

    First commit

---
+++ Abc.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+
+class Abc 
+{
+
+public static void main(String x[])
+
+{
+
+System.out.println("Hello, Good morning");
+
+
+}
+
+
+
+
+}
---
+++ GitApp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+
+class GitApp
+{
+
+public static void main(String x[])
+
+{
+
+System.out.println("GIT DEMO FILE");
+
+
+}
+
+
+
+
+}
